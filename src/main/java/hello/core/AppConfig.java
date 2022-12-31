09f946298bcc873c3231edc9eaae9044a0c5cc94
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-12-31 17:48:29 +0900

    refactor: (AppConfig) Spring을 활용하여 구현

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,29 +1,37 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-    public MemberService memberService(){
+    @Bean
+    public MemberService memberService() {
         return new MemberServiceImpl(MemberRepository());
     }
 
-    private  MemberRepository MemberRepository() {
+    @Bean
+    public MemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(MemberRepository(),discountPolicy());
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
