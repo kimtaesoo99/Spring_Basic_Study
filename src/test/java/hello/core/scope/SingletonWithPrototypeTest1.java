d722f5a1ede35128b1eec410dec8453fea1696ab
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-01 17:40:16 +0900

    refactor: (SingletonWithPrototypeTest1) Provider로 수정

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,11 +44,11 @@ public class SingletonWithPrototypeTest1 {
     static class ClientBean{
 
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
 
         public int logic(){
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); //스프링 컨테이너를 통해 해당 빈을 찾아서 반환
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); //스프링 컨테이너를 통해 해당 빈을 찾아서 반환
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
