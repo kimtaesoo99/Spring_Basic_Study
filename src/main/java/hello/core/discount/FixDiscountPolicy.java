31a5354adbc7d16caf52ab91fcc57d8b36128323
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-01 13:54:38 +0900

    refactor: (FixDiscountPolicy) @primary 사용하기위해 @Qualifier 제거

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,11 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int disCountFixAmount = 1000; //1000원 할인
