8954cfea07128ecfa8872439d5fdc752893a811c
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-01 20:19:07 +0900

    refactor: (LogDemoService) 프록시를 이용하여 리펙토링

--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -2,18 +2,15 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = "+ id);
     }
 }
