7a0f9aca0cdb1a2e596b01fff785a1e6ee2cc9ff
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-01 17:28:59 +0900

    test: (SingletonWithPrototypeTest1) DL기능을 활용하여 프로토타입 테스트

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -34,25 +35,39 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
-    @Scope("singleton") //default값
+    @Scope("singleton")
     static class ClientBean{
 
-        private PrototypeBean prototypeBean; //생성시점에 주입
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); //스프링 컨테이너를 통해 해당 빈을 찾아서 반환
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
     }
+
+//    @Scope("singleton") //default값
+//    static class ClientBean{
+//
+//        private PrototypeBean prototypeBean; //생성시점에 주입
+//
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+//
+//        public int logic(){
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
+//    }
 
     @Scope("prototype")
     static class PrototypeBean{
